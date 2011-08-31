a2c10ee276694f6a91fb1d8e73df619520093c9e
Paul Annesley <paul@annesley.cc>
2011-08-31 12:09:49 +1000

    Completion configured by compinstall.

---
+++ completion.zsh
@@ -0,0 +1,11 @@
+# The following lines were added by compinstall
+
+zstyle ':completion:*' completer _expand _complete _ignored
+zstyle ':completion:*' list-colors ''
+zstyle ':completion:*' menu select=long
+zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
+zstyle :compinstall filename '/Users/paul/.zsh/completion'
+
+autoload -Uz compinit
+compinit
+# End of lines added by compinstall
