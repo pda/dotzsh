b100a10f326c5236eb7256069177e80e9ee2a3c1
Paul Annesley <paul@annesley.cc>
2011-08-31 10:04:01 +1000

    Simple prompt; "user@host ~/path ) " where ")" is unicode parenthesis colored by exit status.

--- prompt.zsh
+++ prompt.zsh
@@ -1,5 +1,5 @@
 bindkey -v
 
-export PROMPT=$'%{\e[0;33m%}%m%{\e[0m%} %~ %{\e[0;%(?.32.31)m%}>%{\e[0m%} '
+export PROMPT=$'%n@%m %~ %{\e[0;%(?.32.31)m%}⸩%{\e[0m%} '
 export RPROMPT=$'%{\e[0;90m%}[%*]%{\e[0m%}'
 
