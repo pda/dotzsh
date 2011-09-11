f2c899bc3e7f7afb34c87fe32f2c69f21e717846
Paul Annesley <paul@annesley.cc>
2011-09-11 19:27:53 +1000

    KEYTIMEOUT=1 (default was 40) to make vi mode feel less laggy.
    By default, pressing escape to exit vi insert mode would wait 400ms for
    a subsequent key to ensure it was not part of an escape sequence or a
    bound multi-character sequence.

--- keyboard.zsh
+++ keyboard.zsh
@@ -1,5 +1,13 @@
 # vi mode
 bindkey -v
 
+# The time the shell waits, in hundredths of seconds, for another key
+# to be pressed when reading bound multi-character sequences.
+#
+# Set to shortest possible delay is 1/100 second.
+# This allows escape sequences like cursor/arrow keys to work,
+# while eliminating the delay exiting vi insert mode.
+KEYTIMEOUT=1
+
 # Ctrl-R for reverse incremental search
 bindkey "^R" history-incremental-search-backward
