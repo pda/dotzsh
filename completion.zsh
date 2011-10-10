89bb90effede2ef3eb2ebb900c92eaa6a89e6fd9
Paul Annesley <paul@annesley.cc>
2011-10-10 13:33:17 +1200

    Removed user-specific path from completion.zsh generated by compinstall.

--- completion.zsh
+++ completion.zsh
@@ -4,7 +4,6 @@ zstyle ':completion:*' completer _expand _complete _ignored
 zstyle ':completion:*' list-colors ''
 zstyle ':completion:*' menu select=long
 zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
-zstyle :compinstall filename '/Users/paul/.zsh/completion'
 
 autoload -Uz compinit
 compinit
