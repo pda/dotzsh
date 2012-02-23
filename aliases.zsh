b44b8af0d113cbc63eba29d33459513d285d4e12
Paul Annesley <paul@annesley.cc>
2012-02-23 11:05:16 +1100

    Alias: vim => reattach-to-user-namespace vim

--- aliases.zsh
+++ aliases.zsh
@@ -1,2 +1,6 @@
 alias pstree="pstree -g 2"
 alias hd="hexdump -C"
+
+# See: https://github.com/ChrisJohnsen/tmux-MacOSX-pasteboard
+(( $+commands[reattach-to-user-namespace] )) &&
+  alias vim="reattach-to-user-namespace vim"
