afb231ba9acb80f0697eafc930a7e9b7172b566f
Paul Annesley <paul@annesley.cc>
2012-04-17 15:19:46 +1000

    Alias diff=colordiff if the latter exists.

--- aliases.zsh
+++ aliases.zsh
@@ -4,3 +4,6 @@ alias hd="hexdump -C"
 # See: https://github.com/ChrisJohnsen/tmux-MacOSX-pasteboard
 (( $+commands[reattach-to-user-namespace] )) &&
   alias vim="reattach-to-user-namespace vim"
+
+(( $+commands[colordiff] )) &&
+  alias diff="colordiff"
