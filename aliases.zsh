b50e9e2dacb7786619fcf1bcf42ee9e22014621a
Paul Annesley <paul@annesley.cc>
2012-04-17 15:20:54 +1000

    mkts function; create & change to a timestamp dir e.g. "20120417-1521".

--- aliases.zsh
+++ aliases.zsh
@@ -1,6 +1,12 @@
 alias pstree="pstree -g 2"
 alias hd="hexdump -C"
 
+# Make and change to a timestamp directory.
+mkts() {
+  local ts=`date +%Y%m%d-%H%m%S`
+  mkdir $ts && cd $ts
+}
+
 # See: https://github.com/ChrisJohnsen/tmux-MacOSX-pasteboard
 (( $+commands[reattach-to-user-namespace] )) &&
   alias vim="reattach-to-user-namespace vim"
