36cd1cee77259865f856a3297b2e07d863f33d09
Paul Annesley <paul@annesley.cc>
2013-04-02 12:37:38 +1100

    More Ruby GC options in environment.

--- ruby.zsh
+++ ruby.zsh
@@ -1,5 +1,9 @@
-# Ruby 1.9.3 performance
-export RUBY_GC_MALLOC_LIMIT=64000000
+# Ruby 1.9.3 performance: FEED ME RAM.
+export RUBY_HEAP_MIN_SLOTS=1000000
+export RUBY_HEAP_SLOTS_INCREMENT=1000000
+export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
+export RUBY_GC_MALLOC_LIMIT=100000000
+export RUBY_HEAP_FREE_MIN=500000
 
 # rbenv
 eval "$(rbenv init -)"
