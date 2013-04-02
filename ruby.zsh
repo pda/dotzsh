939f8f385aa8404765f5f5398993f588b9bf6430
Paul Annesley <paul@annesley.cc>
2013-04-02 12:57:52 +1100

    chruby loaded in ruby.zsh

--- ruby.zsh
+++ ruby.zsh
@@ -9,5 +9,12 @@ export RUBY_HEAP_FREE_MIN=500000
 (( $+commands[rbenv] )) &&
   eval "$(rbenv init -)"
 
+# chruby
+CHRUBY_INIT=/usr/local/share/chruby/chruby.sh
+CHRUBY_AUTO=/usr/local/share/chruby/auto.sh
+[ -f $CHRUBY_INIT ] && source $CHRUBY_INIT
+[ -f $CHRUBY_AUTO ] && source $CHRUBY_AUTO
+unset CHRUBY_INIT CHRUBY_AUTO
+
 # Bundler
 alias bx="bundle exec"
