790bbabf270ca160154e1e69f4b585e73f9a4359
Paul Annesley <paul@annesley.cc>
2013-07-01 21:11:13 +1000

    rc.zsh as anonymous function; don't leak vars into global scope.

--- rc.zsh
+++ rc.zsh
@@ -1,16 +1,22 @@
-# sources to load first
-pre_sources=( options )
+function {
 
-# sources to load last
-post_sources=()
+  local pre_sources auto_sources post_sources
 
-# load remaining sources automatically
-auto_sources=(`for f in ~/.zsh/*.zsh; do basename $f .zsh; done`)
+  # sources to load first
+  pre_sources=( options )
 
-for source in rc $pre_sources $post_sources; do
-  auto_sources[(i)$source]=""
-done
+  # sources to load last
+  post_sources=()
 
-for source in $pre_sources $auto_sources $post_sources; do
-  source ~/.zsh/$source.zsh
-done
+  # load remaining sources automatically
+  auto_sources=(`for f in ~/.zsh/*.zsh; do basename $f .zsh; done`)
+
+  for source in rc $pre_sources $post_sources; do
+    auto_sources[(i)$source]=""
+  done
+
+  for source in $pre_sources $auto_sources $post_sources; do
+    source ~/.zsh/$source.zsh
+  done
+
+}
