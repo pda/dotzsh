bf36e3dacc5c82739cada05405cdf47a9f43ea6f
Paul Annesley <paul@annesley.cc>
2014-01-02 17:35:47 -0800

    Remove Ruby 1.9.3 "optimizations" from ENV; ruby 2.1 hates them.
    Warning emitted by Ruby 2.1.0p0:
    
        ~/.rubies/ruby-2.1.0/bin/ruby: warning: RUBY_HEAP_MIN_SLOTS is obsolete. Use RUBY_GC_HEAP_INIT_SLOTS instead.

--- ruby.zsh
+++ ruby.zsh
@@ -1,10 +1,3 @@
-# Ruby 1.9.3 performance: FEED ME RAM.
-export RUBY_HEAP_MIN_SLOTS=1000000
-export RUBY_HEAP_SLOTS_INCREMENT=1000000
-export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
-export RUBY_GC_MALLOC_LIMIT=100000000
-export RUBY_HEAP_FREE_MIN=500000
-
 # rbenv
 (( $+commands[rbenv] )) &&
   eval "$(rbenv init -)"
