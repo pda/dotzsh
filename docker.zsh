3c0a53636c16310dd1fe7d068cd3944d7e0a0e0f
Paul Annesley <paul@annesley.cc>
2014-10-16 18:23:20 -0700

    DOCKER_HOST port changed to 2375, was 4243.

--- docker.zsh
+++ docker.zsh
@@ -1,5 +1,5 @@
 # Use tcp://localhost for docker if no local socket present.
 # e.g. Vagrant may be forwarding localhost:4243 to a docker host VM.
 if [[ ! -S /var/run/docker.sock ]]; then
-  export DOCKER_HOST="tcp://localhost:4243"
+  export DOCKER_HOST="tcp://localhost:2375"
 fi
