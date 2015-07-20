1d637a1c00e4c2bf890f200ece62f2ae71fea537
Paul Annesley <paul@annesley.cc>
2015-07-19 20:51:16 -0700

    java.sh: --failfast handles glorious absence of JVM.

--- java.zsh
+++ java.zsh
@@ -1,3 +1,10 @@
-if [ -x /usr/libexec/java_home ]; then
-  export JAVA_HOME=$(/usr/libexec/java_home)
-fi
+set_java_home() {
+  if [ -x /usr/libexec/java_home ]; then
+    local tmp=$(/usr/libexec/java_home --failfast 2>/dev/null)
+    if [ -n "$tmp" ]; then
+      export JAVA_HOME=$(/usr/libexec/java_home)
+    fi
+  fi
+}
+
+set_java_home
