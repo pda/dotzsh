12499a66dcc8005cccf35096cc51cdae9b4d02d0
Paul Annesley <paul@annesley.cc>
2015-07-19 20:56:13 -0700

    Support for ~/.zsh/completion/* files.

--- completion.zsh
+++ completion.zsh
@@ -5,6 +5,8 @@ zstyle ':completion:*' list-colors ''
 zstyle ':completion:*' menu select=long
 zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
 
+fpath=(~/.zsh/completion $fpath)
+
 autoload -Uz compinit
 compinit
 # End of lines added by compinstall
