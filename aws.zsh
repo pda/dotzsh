945c6bb8d45805fd19ccbd9eaf1b1ecb93d55dab
Paul Annesley <paul@annesley.cc>
2015-07-19 20:54:32 -0700

    aws.zsh: remove AWS_CLOUDFORMATION_HOME.

--- aws.zsh
+++ aws.zsh
@@ -1,4 +1,2 @@
-export AWS_CLOUDFORMATION_HOME="/usr/local/AWSCloudFormation-1.0.12"
-export PATH="$AWS_CLOUDFORMATION_HOME/bin:$PATH"
 export EC2_KEY_NAME=$(cat $HOME/.aws/ec2_key_name 2>/dev/null)
 export AWS_CREDENTIAL_FILE="$HOME/.aws/credentials"
