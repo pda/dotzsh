ed163903d02baca101b9f72d79ce0dc220ae23df
Paul Annesley <paul@annesley.cc>
2015-10-12 15:45:14 -0700

    ruby: disable chruby's auto mode.

--- ruby.zsh
+++ ruby.zsh
@@ -1,9 +1,7 @@
 # chruby
 CHRUBY_INIT=/usr/local/share/chruby/chruby.sh
-CHRUBY_AUTO=/usr/local/share/chruby/auto.sh
 [ -f $CHRUBY_INIT ] && source $CHRUBY_INIT
-[ -f $CHRUBY_AUTO ] && source $CHRUBY_AUTO
-unset CHRUBY_INIT CHRUBY_AUTO
+unset CHRUBY_INIT
 
 # Bundler
 alias bx="bundle exec"
