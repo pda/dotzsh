db005d3d4d617d2e97fcf9f476fcfc9da4bbabdc
Paul Annesley <paul@annesley.cc>
2016-12-19 13:17:15 +1100

    vim: no reattach-to-user-namespace; assume already run if needed.

--- vim.zsh
+++ vim.zsh
@@ -1,24 +1,12 @@
-vim()
-{
+vim() {
   # Save current stty options.
   local STTYOPTS="$(stty -g)"
 
   # Disable intercepting of ctrl-s and ctrl-q as flow control.
   stty stop '' -ixoff -ixon
 
-  # Execute vim.
-  vim_command "$@"
+  command vim "$@"
 
   # Restore saved stty options.
   stty "$STTYOPTS"
-}
-
-vim_command()
-{
-  if (( $+commands[reattach-to-user-namespace] )); then
-    # See: https://github.com/ChrisJohnsen/tmux-MacOSX-pasteboard
-    command reattach-to-user-namespace vim "$@"
-  else
-    command vim "$@"
-  fi
 }
