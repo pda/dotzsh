71170ce6f7d94d68ef5c8c3bb219f0771d401e6a
Paul Annesley <paul@annesley.cc>
2018-05-02 15:45:27 +1000

    aws-vault: avsh function to get a 1h shell with expiry notification

--- aws-vault.zsh
+++ aws-vault.zsh
@@ -1,3 +1,20 @@
 if [[ -n $AWS_VAULT ]]; then
   PROMPT="%F{242}[aws:$AWS_VAULT]%f $PROMPT"
 fi
+
+# get an aws-vault shell for an hour, notify when expired
+avsh() {
+  seconds=3600
+  setopt LOCAL_OPTIONS NO_NOTIFY NO_MONITOR
+  if [[ $# -ne 1 ]]; then echo "Usage: $0 <profile>"; return; fi
+  (
+    n=0; stop=0
+    trap "stop=1" TERM
+    until [[ $n -ge $seconds || $stop -eq 1 ]]; do n=$[$n+1]; sleep 1; done
+    [[ $stop -eq 0 ]] && echo -e "\naws-vault session expired"
+  )&
+  subshell="$!"
+  disown
+  aws-vault exec --assume-role-ttl="$seconds"s "$1"
+  kill -TERM "$subshell" 2>/dev/null && true
+}
