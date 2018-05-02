b08eba0c24ec6910e9815498a60ec43cbd542bf9
Paul Annesley <paul@annesley.cc>
2018-05-02 15:43:56 +1000

    aliases: `ssh` does s/tmux/xterm/ on $TERM env.
    Because many remote systems understand xterm-256color but not
    tmux-256color and that's fine.

--- aliases.zsh
+++ aliases.zsh
@@ -2,6 +2,9 @@ alias pstree="pstree -g 2"
 alias hd="hexdump -C"
 alias dc="docker-compose"
 
+# most remote systems don't understand tmux-256color etc.
+alias ssh="TERM=${TERM/tmux/xterm} ssh"
+
 # RFC 822 date format; HTTP etc. Not built in to BSD `date`.
 alias date822="date '+%a, %d %h %Y %H:%M:%S %z'"
 alias date8601="date -u '+%Y-%m-%dT%H:%M:%SZ'"
