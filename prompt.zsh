13a022bc7443d327b5bf02a2fa9ef447fa261dc0
Paul Annesley <paul@annesley.cc>
2021-03-01 14:15:39 +1100

    traditional $ prompt (# for root); no more fancy unicode.
    It was a good decade since b100a10f326c5236eb7256069177e80e9ee2a3c1 but
    all good things must end. Too often it broken when running zsh config on
    strange systems / terminals.

--- prompt.zsh
+++ prompt.zsh
@@ -3,12 +3,4 @@
 # See: man zshzle
 bindkey -v
 
-# Fancy UTF-8 if the terminal can handle it, e.g. iTerm2 and uxterm.
-# Boring ASCII for xterm where XTERM_LOCALE="C".
-if [[ -z $XTERM_LOCALE || $XTERM_LOCALE =~ "UTF-8" ]]; then
-  PROMPTCHAR="❯"
-else
-  PROMPTCHAR=">"
-fi
-
-export PROMPT=$'%n@%m %~ %{\e[2;%(?.32.31)m%}$PROMPTCHAR%{\e[0m%} '
+export PROMPT=$'%n@%m %~ %{\e[2;%(?.32.31)m%}%(!.#.\$)%{\e[0m%} '
