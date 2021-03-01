5568f37960788229e3e8e193909ad48c141df1ab
Paul Annesley <paul@annesley.cc>
2021-03-01 14:23:12 +1100

    ripgrep & fzf are friends. Include hidden files, but not .git

--- environment.zsh
+++ environment.zsh
@@ -10,3 +10,12 @@ export PROMPT_EOL_MARK=💀
 HISTFILE=~/.zsh_history
 HISTSIZE=100000
 SAVEHIST=HISTSIZE
+
+# Load ripgrep (rg) config from disk.  At time of writing, it sets --hidden
+# --glob=!.git so that hidden files other than the git repository internals are
+# included in search, including in nvim/fzf.
+export RIPGREP_CONFIG_PATH="$HOME/.config/ripgreprc"
+
+# Use `ripgrep` instead of the default `find` command as fzf input.
+# This makes nvim/fzf faster & better, especially when combined with RIPGREP_CONFIG_PATH.
+export FZF_DEFAULT_COMMAND="rg --files"
